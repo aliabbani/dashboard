9a739645315ae85e03f55dab242b44ace7733a2d
Ali Abbani <aliabbani08@gmail.com>
2022-08-11 22:15:37 +0300

    add featuredInfo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./app.css";
+import Home from "./pages/home/home";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="other">other</div>
+        <Home />
       </div>
     </Router>
   );
---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
+}
+
+export default Home;
