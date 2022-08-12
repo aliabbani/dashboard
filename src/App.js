78e2bb42229de50b67984092ae673c3b74c8e642
Ali Abbani <aliabbani08@gmail.com>
2022-08-12 15:34:37 +0300

    add newUser, create user page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./app.css";
 import Home from "./pages/home/Home.js";
 import UserList from "./pages/userList/UserList.js";
 import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/welcome" element={<Home />} />
           <Route path="/users" element={<UserList />} />
           <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser />} />
         </Routes>
       </div>
     </Router>
