5f305e038d2d744ef9f969fa4a64f5af98656fcb
Ali Abbani <aliabbani08@gmail.com>
2022-08-12 15:04:05 +0300

    Add edit user page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./app.css";
 import Home from "./pages/home/Home.js";
 import UserList from "./pages/userList/UserList.js";
+import User from "./pages/user/User";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path="/welcome" element={<Home />} />
           <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
         </Routes>
       </div>
     </Router>
